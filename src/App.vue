87dc4cfae3d3f800f312a66737135c6bd5c24398
ZQZS <qwy6214@outlook.com>
2022-03-31 17:10:55 +0800

    vuetest

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+<h1>hello world</h1>
+  <button>点击</button>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const {VueLoaderPlugin} = require('vue-loader')
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        alias: {
+          'vue$': 'vue/dist/vue.esm.js' // 用 webpack 1 时需用 'vue/dist/vue.common.js'
+        }
+      },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.m?js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader','scss-loader']
+            },
+            {
+                test:/\.(png|jpe?g|gif|svg|webp)$/,
+                type:'asset/resource'
+            }
+        ]
+    },
+    plugins:[
+        new VueLoaderPlugin()
+    ]
+}
